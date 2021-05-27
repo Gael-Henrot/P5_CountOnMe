c5c84b9e55835fb5c93ff801b806290526478839
HENROT Gaël <gael.henrot@expleogroup.com>
2021-05-27 11:17:21 +0200

    Initial Commit

---
+++ CountOnMe/Model/Calcul.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  CountOnMe
+//
+//  Created by Gaël HENROT on 26/05/2021.
+//  Copyright © 2021 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class Calcul {
+    
+}
